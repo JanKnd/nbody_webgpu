f8d64005e943e4dbc074a5445baaa86a653dbb5d
Jan Kunde <jan.kunde@wetteronline.de>
2025-04-01 08:59:41 +0200

    Window setup

---
+++ src/lib.rs
@@ -0,0 +1,192 @@
+use std::iter;
+
+use wgpu::{core::device::queue, Surface};
+use winit::{
+    dpi::PhysicalSize, event::*, event_loop::EventLoop, keyboard::{KeyCode, PhysicalKey}, window::{Window, WindowBuilder}
+};
+
+struct State<'a> {
+    surface: wgpu::Surface<'a>,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    config: wgpu::SurfaceConfiguration,
+    size: winit::dpi::PhysicalSize<u32>,
+    window: &'a Window,
+}
+
+impl<'a> State<'a> {
+    async fn new(window: &'a Window) -> State<'a> {
+        let size = window.inner_size();
+
+        let instance = wgpu::Instance::new(&wgpu::InstanceDescriptor {
+            backends: wgpu::Backends::PRIMARY,
+            ..Default::default()
+        });
+        
+        let surface = instance.create_surface(window).expect("couldn't create window");
+
+        let adapter = instance.request_adapter(
+            &wgpu::RequestAdapterOptionsBase {
+                power_preference: wgpu::PowerPreference::default(),
+                force_fallback_adapter: false,
+                compatible_surface: Some(&surface)
+            }
+        ).await.expect("could not get adapter");
+
+        let (device, queue) = adapter.request_device(&wgpu::DeviceDescriptor {
+            label: Some("Device"),
+            required_features: wgpu::Features::empty(),
+            required_limits: wgpu::Limits::default(),
+            memory_hints: Default::default()},
+            None,
+        ).await.expect("Could not get device and queue");
+
+        let surface_caps = surface.get_capabilities(&adapter);
+
+        let surface_format = surface_caps.formats.iter()
+            .find(|f| f.is_srgb())
+            .copied()
+            .unwrap_or(surface_caps.formats[0]);
+
+        let config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format: surface_format,
+            width: size.width,
+            height: size.height,
+            present_mode: surface_caps.present_modes[0],
+            desired_maximum_frame_latency: 2,
+            alpha_mode: surface_caps.alpha_modes[0],
+            view_formats: vec![],
+        };
+
+        Self{
+            surface,
+            device,
+            queue,
+            config,
+            size,
+            window,
+        }
+    } 
+
+    pub fn window(&self) -> &Window {
+        &self.window
+    }
+
+    fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
+        if new_size.width > 0 && new_size.height > 0 {
+            self.size = new_size;
+            self.config.width = new_size.width;
+            self.config.height = new_size.height;
+            self.surface.configure(&self.device, &self.config);
+        }
+    }
+
+    fn input(&mut self, event: &WindowEvent) -> bool {
+        false
+    }
+
+    fn update(&mut self) {}
+
+    fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
+        let output = self.surface.get_current_texture()?;
+        let view = output.texture.create_view(&wgpu::TextureViewDescriptor::default());
+
+        let mut encoder = self.device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
+            label: Some("render encoder"),
+        });
+
+        {
+            let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                label: Some("render pass"),
+                color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                        view: &view,
+                        resolve_target: None,
+                        ops: wgpu::Operations {
+                            load: wgpu::LoadOp::Clear(
+                                wgpu::Color {
+                                    r: 0.0,
+                                    g: 0.0,
+                                    b: 0.0,
+                                    a: 0.0
+                                }
+                            ),
+                            store: wgpu::StoreOp::Store,
+                        }
+                    }
+                )],
+                depth_stencil_attachment: None,
+                timestamp_writes: None,
+                occlusion_query_set: None,
+            });
+        }
+
+        self.queue.submit(iter::once(encoder.finish()));
+        output.present();
+
+        Ok(())
+    }
+}
+
+
+
+pub async fn run() {
+    env_logger::init();
+    let event_loop = EventLoop::new().unwrap();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+    
+    let mut state = State::new(&window).await;
+    let mut surface_configured = false;
+
+    let _ = event_loop.run(move |event, control_flow| match event {
+        Event::WindowEvent {
+            ref event,
+            window_id,
+        } if window_id == state.window.id() => match event {
+            WindowEvent::CloseRequested
+            | WindowEvent::KeyboardInput {
+                event:
+                    KeyEvent {
+                        state: ElementState::Pressed,
+                        physical_key: PhysicalKey::Code(KeyCode::Escape),
+                        ..
+                    },
+                    
+                ..
+            } => control_flow.exit(),
+            WindowEvent::Resized(physical_size) => {
+                state.resize(*physical_size);
+                surface_configured = true;
+            }
+            WindowEvent::RedrawRequested => {
+                state.window().request_redraw();
+
+                if !surface_configured {
+                    return;
+                }
+
+                state.update();
+                match state.render() {
+                    Ok(_) => {},
+                    Err(
+                        wgpu::SurfaceError::Lost | wgpu::SurfaceError::Outdated,
+                    ) => state.resize(state.size),
+                    Err(
+                        wgpu::SurfaceError::OutOfMemory | wgpu::SurfaceError::Other
+                    ) => {
+                        log::error!("OutOfMemory");
+                        control_flow.exit();
+                    }
+                    Err(
+                        wgpu::SurfaceError::Timeout
+                    ) => {
+                        log::warn!("Surface Timeout")
+                    }
+                }
+            }
+            _ => {}
+        },
+        _ => {}
+    });
+}
+
---
+++ src/main.rs
@@ -0,0 +1,5 @@
+use nbody_webgpu::run;
+
+fn main(){
+    pollster::block_on(run());
+}
